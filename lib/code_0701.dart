b97024e6580e7896eb34c22caae4506422b8467e
hyun471 <khs101ttl@gmail.com>
2025-07-01 09:48:45 +0900

    code_0701 추가

---
+++ lib/code_0701.dart
@@ -0,0 +1,67 @@
+void main() {
+  final arr = [7, 9, 2, 3, 1];
+  print("시작 전 : $arr");
+  for (var i = 0; i < arr.length - 1; i++) {
+    print("$i번째 라운드");
+    for (var j = 0; j < arr.length - 1; j++) {
+      print("반복문");
+      final a = arr[j];
+      final b = arr[j + 1];
+      if (a > b) {
+        arr[j] = b;
+        arr[j + 1] = a;
+      }
+    }
+  }
+  print("시작 후 : $arr");
+}
+
+void bubbleSort(List<int> arr) {
+  for (var i = 0; i < arr.length - 1; i++) {
+    for (var j = 0; j < arr.length - 1; j++) {
+      final a = arr[j];
+      final b = arr[j + 1];
+      if (a > b) {
+        arr[j] = b;
+        arr[j + 1] = a;
+      }
+    }
+  }
+}
+
+
+  //버블정렬
+  // [7, 9, 2, 3, 1]
+  // 첫번째 라운드
+  // 0번째, 1번째 비교 후 교환 [7, 9, 2, 3, 1]
+  // 1번째, 2번째 비교 후 교환 [7, 2, 9, 3, 1]
+  // 2번째, 3번째 비교 후 교환 [7, 2, 3, 9, 1]
+  // 3번째, 4번째 비교 후 교환 [7, 2, 3, 1, 9]
+  // [7, 2, 3, 1, 9]
+  // 두번째 라운드
+  // 0번째, 1번째 비교 후 교환 [2, 7, 3, 1, 9]
+  // 1번째, 2번째 비교 후 교환 [2, 3, 7, 1, 9]
+  // 2번째, 3번째 비교 후 교환 [2, 3, 1, 7, 9]
+  // [2, 3, 1, 7, 9]
+  // 세번째 라운드
+  // 0번째, 1번째 비교 후 교환 [2, 3, 1, 7, 9]
+  // 1번째, 2번째 비교 후 교환 [2, 1, 3, 7, 9]
+  // 2번째, 3번째 비교 후 교환 [1, 2, 3, 7, 9]
+  // [2, 3, 1, 7, 9]
+
+
+
+
+  /* List<int> bubble = [7, 9, 2, 3, 1];
+  for (int j = 5; j == 0; j -= 1) {
+    for (int i = 0; i; i++) {
+      if (bubble[i] < bubble[i + 1]) {
+        int number = bubble[i];
+        bubble[i] = bubble[i + 1];
+        bubble[i + 1] = number;
+        print("$bubble");
+      }
+    }
+  }
+*/
+  
--- lib/dart_morning_coding.dart
+++ lib/dart_morning_coding.dart
@@ -1,3 +1,37 @@
-int calculate() {
-  return 6 * 7;
+void main() {
+  //
+  int result = add(2148201489201, 42819401809428019);
+  int result2 = add(result, 10000);
+  // 클래스 생성한다는 건 => 설계도 대로 사용할 수 있게 실제값을 만드는 것
+  
+  Marine(100, 200);
+  Marine m1 = Marine(100, 200);
+  Marine m2 = Marine(100, 200);
+  Marine m3 = Marine(100, 200);
+  m1.talk();
 }
+
+// 함수의 변환 타입
+// 숫자 두개를 입력받아서 더한값을 구하는 함수
+// 함수의 반환타입 적는 이유 : 함수의 결과를 변수에 담아서 쓰려고!!!
+int add(int a, int b) {
+  return a + b;
+}
+
+// 클래스 및 객체
+class Marine {
+  int hp;
+  int mp;
+  // 클래스를 객체화 해주는 특별한 함수
+  Marine(this.hp, this.mp);
+  void talk() {
+    print("You wanna piece of me boy");
+  }
+}
+
+class Scv{
+  // 생성자를 개발자가 정의하지 않으면 기본 생성자가 생김(Scv())
+}
+
+
+// 객체 생성 
